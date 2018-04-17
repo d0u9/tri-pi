129f8c85b7f70559b96e93b78d2b31ee7551e05b
d0u9 <d0u9.su@outlook.com>
2018-04-17 21:57:56 +0800

    add simple menu display

--- rest_request/main.py
+++ rest_request/main.py
@@ -5,9 +5,15 @@ import RPi.GPIO as GPIO
 import time
 import requests
 import logging
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
 
 from logging.handlers import RotatingFileHandler
 from threading import Timer
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+from PIL import ImageMath
 
 LED_R = 16
 LED_G = 20
@@ -18,11 +24,19 @@ BTN_2 = 13  # Videso
 BTN_3 = 19  # Games
 BTN_4 = 26  # Backup
 
+# Raspberry Pi pin configuration:
+# We use hardware SPI
+RST = 24
+DC = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
 TIME_TO_TURN_LEDS_OFF = 5
 TIMER = None
+LOGO_SHOW_TIME = 1
+FONT_SIZE = 8
 
 REST_SERVER = 'http://tri-server:8080/'
-
 
 log_formatter = logging.Formatter('[%(asctime)s] [%(process)d] [%(levelname)s] %(message)s', '%Y-%m-%d %H:%M:%S %z')
 
@@ -93,6 +107,21 @@ def handle(pin):
 
 if __name__ == '__main__':
     logger.info('Starting...')
+
+    disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
+    disp.begin()
+
+    # Get display width and height.
+    width = disp.width
+    height = disp.height
+
+    disp.clear()
+    disp.display()
+
+    image = Image.open('Logo2.bmp').convert('1')
+    disp.image(image)
+    disp.display()
+
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     GPIO.setup([BTN_1, BTN_2, BTN_3, BTN_4], GPIO.IN, pull_up_down=GPIO.PUD_OFF)
@@ -103,6 +132,42 @@ if __name__ == '__main__':
     GPIO.add_event_detect(BTN_3, GPIO.FALLING, handle)
     GPIO.add_event_detect(BTN_4, GPIO.FALLING, handle)
 
+    time.sleep(LOGO_SHOW_TIME)
+    disp.clear()
+
+    font = ImageFont.truetype('font.ttf', FONT_SIZE)
+
+    image = Image.new('1', (width, height))
+    draw = ImageDraw.Draw(image)
+
+    menu = ['Get up early',
+            'Goto school',
+            'Rest',
+            'Study',
+            'Excise',
+            'Sleep']
+
+    for i in range(0, 6):
+        #draw.text((2, 2 + (FONT_SIZE + 2) * i), 'ABCDEF ?*&^ abcedf', font=font, fill=1)
+        draw.text((2, 2 + (FONT_SIZE + 2) * i), menu[i], font=font, fill=1)
+
+    disp.image(image)
+    disp.display()
+
+    time.sleep(1)
+
     while True:
-        time.sleep(1e6)
+        for i in range(0, 6):
+            box = (0, 3 + (10 * i), width-1, 12 + (10 * i))
+            bef = image.crop(box)
+            aft = ImageMath.eval('255-(a)', a=bef)
+            image.paste(aft, box=box)
+            disp.image(image)
+            disp.display()
+            time.sleep(1)
+            image.paste(bef, box=box)
+            disp.image(image)
+            disp.display()
+
+        time.sleep(1)
 
