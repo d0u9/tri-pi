20b519a291e92839ccceb5de26222b89f0507edf
d0u9 <d0u9.su@outlook.com>
2018-04-24 21:03:47 +0800

    fix bug

--- rest_request/app_about.py
+++ rest_request/app_about.py
@@ -14,27 +14,28 @@ class AboutApp(LineStringApp):
     def __init__(self, display, config):
         super(AboutApp, self).__init__(display, config)
         self.draw_image()
+        self.font = 'font.ttf'
 
     def draw_image(self):
         self.set(font_size=16, box=(5, 1, 128, 20), line_height=18)
 
         self.redraw('About', clear=True)
-        self.set('fix.ttf', font_size=8, box=(5, 23, 128-5, 32), line_height=8)
+        self.set(self.font, font_size=8, box=(5, 23, 128-5, 32), line_height=10)
         self.redraw('Version: dev')
 
-        self.set('fix.ttf', font_size=8, box=(5, 33, 128-5, 42), line_height=8)
+        self.set(self.font, font_size=8, box=(5, 33, 128-5, 42), line_height=10)
         self.redraw('Author: d0u9')
 
         kernel = check_output(['/bin/uname', '-r']).decode('ascii').strip()
-        self.set('fix.ttf', font_size=8, box=(5, 41, 128-5, 50), line_height=8)
+        self.set(self.font, font_size=8, box=(5, 43, 128-5, 52), line_height=10)
         self.redraw('Kernel: {}'.format(kernel))
 
         os_name = check_output(['/bin/uname', '-s']).decode('ascii').strip()
-        self.set('fix.ttf', font_size=8, box=(5, 51, 128-5, 60), line_height=8)
+        self.set(self.font, font_size=8, box=(5, 53, 128-5, 62), line_height=10)
         self.redraw('OS: {}'.format(os_name))
 
         arch = check_output(['/bin/uname', '-m']).decode('ascii').strip()
-        self.set('fix.ttf', font_size=8, box=(5, 61, 128-5, 70), line_height=8)
+        self.set(self.font, font_size=8, box=(5, 63, 128-5, 72), line_height=10)
         self.redraw('Arch: {}'.format(arch))
 
         self.refresh()
